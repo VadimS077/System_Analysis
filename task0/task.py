73fb98e0dc37c374f61a8a617aa16028d7bfb552
Vadim Soin <vadimiki17@mail.ru>
2025-10-09 11:52:53 +0300

    lab2

--- task0/task.py
+++ task0/task.py
@@ -1,4 +1,4 @@
-def solve(file_path):
+def main(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         lines = [line.strip() for line in f if line.strip()]
 
@@ -30,12 +30,12 @@ def solve(file_path):
         print(' '.join(str(x) for x in row))
 
 
-if __name__ == "__main__":
-    import sys
-    import os
 
-    input_path = ''
-    if len(sys.argv) > 1:
-        input_path = sys.argv[1]
+import sys
+import os
 
-    solve(input_path)
+input_path = ''
+if len(sys.argv) > 1:
+    input_path = sys.argv[1]
+
+main(input_path)
